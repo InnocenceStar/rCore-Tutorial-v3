fd3b5553cb8940593e6df355a5b9c971f20d6398
Yifan Wu <shinbokuow@163.com>
2026-06-07 17:26:33 +0800

    refactor: use linker symbol addresses in kernel (#171)

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -8,14 +8,14 @@ pub fn get_num_app() -> usize {
     unsafe extern "C" {
         safe fn _num_app();
     }
-    unsafe { (_num_app as *const () as usize as *const usize).read_volatile() }
+    unsafe { (linker_symbol_addr!(_num_app) as *const usize).read_volatile() }
 }
 /// get applications data
 pub fn get_app_data(app_id: usize) -> &'static [u8] {
     unsafe extern "C" {
         safe fn _num_app();
     }
-    let num_app_ptr = _num_app as *const () as usize as *const usize;
+    let num_app_ptr = linker_symbol_addr!(_num_app) as *const usize;
     let num_app = get_num_app();
     let app_start = unsafe { core::slice::from_raw_parts(num_app_ptr.add(1), num_app + 1) };
     assert!(app_id < num_app);
@@ -34,7 +34,7 @@ lazy_static! {
         unsafe extern "C" {
             safe fn _app_names();
         }
-        let mut start = _app_names as *const () as usize as *const u8;
+        let mut start = linker_symbol_addr!(_app_names) as *const u8;
         let mut v = Vec::new();
         unsafe {
             for _ in 0..num_app {
