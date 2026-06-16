9d1e82b4bafa9bd626388e82629e955bb7cbb8c0
wyfcyx <wyfcyx@users.noreply.github.com>
2026-06-16 07:13:16 +0000

    deploy: 74bf51ce2b8c5b4a9687730282847a9dbc5053e7

--- ch7/trait.impl/core/default/trait.Default.js
+++ ch7/trait.impl/core/default/trait.Default.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"os/task/action/struct.SignalAction.html\" title=\"struct os::task::action::SignalAction\">SignalAction</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"os/task/action/struct.SignalActions.html\" title=\"struct os::task::action::SignalActions\">SignalActions</a>"]]]]);
+    const implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"os/task/action/struct.SignalAction.html\" title=\"struct os::task::action::SignalAction\">SignalAction</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"os/task/action/struct.SignalActions.html\" title=\"struct os::task::action::SignalActions\">SignalActions</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[595]}
+//{"start":59,"fragment_lengths":[597]}
--- ch7/trait.impl/os/mm/frame_allocator/trait.FrameAllocator.js
+++ ch7/trait.impl/os/mm/frame_allocator/trait.FrameAllocator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["os",[]]]);
+    const implementors = Object.fromEntries([["os",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[9]}
+//{"start":59,"fragment_lengths":[9]}
