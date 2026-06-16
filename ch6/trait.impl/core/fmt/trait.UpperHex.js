5f9ec4314e12e69be402bcaee985e06e8ae3a241
wyfcyx <wyfcyx@users.noreply.github.com>
2026-06-16 07:15:34 +0000

    deploy: c17b72575bc6a5d20dd5d5cf08e2cdb7b7a0d0a9

--- ch6/trait.impl/core/fmt/trait.UpperHex.js
+++ ch6/trait.impl/core/fmt/trait.UpperHex.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"os/fs/struct.OpenFlags.html\" title=\"struct os::fs::OpenFlags\">OpenFlags</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"os/mm/page_table/struct.PTEFlags.html\" title=\"struct os::mm::page_table::PTEFlags\">PTEFlags</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"os/mm/struct.MapPermission.html\" title=\"struct os::mm::MapPermission\">MapPermission</a>"]]]]);
+    const implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"os/fs/struct.OpenFlags.html\" title=\"struct os::fs::OpenFlags\">OpenFlags</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"os/mm/page_table/struct.PTEFlags.html\" title=\"struct os::mm::page_table::PTEFlags\">PTEFlags</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"os/mm/struct.MapPermission.html\" title=\"struct os::mm::MapPermission\">MapPermission</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[817]}
+//{"start":59,"fragment_lengths":[820]}
