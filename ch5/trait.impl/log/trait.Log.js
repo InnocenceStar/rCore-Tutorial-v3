69685b314c69948b22ac79c95521cc4ee44bbc04
wyfcyx <wyfcyx@users.noreply.github.com>
2026-06-28 05:17:07 +0000

    deploy: e95047a300bfcebcb1ef9037443212cfa4d155c8

--- ch5/trait.impl/log/trait.Log.js
+++ ch5/trait.impl/log/trait.Log.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.32/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"os/logging/struct.SimpleLogger.html\" title=\"struct os::logging::SimpleLogger\">SimpleLogger</a>",0]]]]);
+    const implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.33/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"os/logging/struct.SimpleLogger.html\" title=\"struct os::logging::SimpleLogger\">SimpleLogger</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
