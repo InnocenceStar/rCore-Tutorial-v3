d04b1744f98d60b68884da6f4d41108e06cb5b01
Yifan Wu <shinbokuow@163.com>
2026-07-02 22:37:06 +0800

    refactor: reserve pid zero (#174)

--- os/src/task/pid.rs
+++ os/src/task/pid.rs
@@ -12,7 +12,7 @@ struct PidAllocator {
 impl PidAllocator {
     pub fn new() -> Self {
         PidAllocator {
-            current: 0,
+            current: 1,
             recycled: Vec::new(),
         }
     }
