51a69ab4d0261fbde370dae77466636440a37673
wyfcyx <wyfcyx@users.noreply.github.com>
2026-07-03 14:52:14 +0000

    deploy: 85fe952a04c4b671fef1beb2f470591ae7ea5123

--- ch1/trait.impl/core/fmt/trait.Write.js
+++ ch1/trait.impl/core/fmt/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"os/console/struct.Stdout.html\" title=\"struct os::console::Stdout\">Stdout</a>",0]]]]);
+    const implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"os/console/struct.Stdout.html\" title=\"struct os::console::Stdout\">Stdout</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
