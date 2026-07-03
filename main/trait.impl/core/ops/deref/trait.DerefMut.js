cdc654c54f885bc987441d67bfa125fc16f2407d
wyfcyx <wyfcyx@users.noreply.github.com>
2026-07-03 14:52:23 +0000

    deploy: c91bd3752b53ff48555aef4e3c7b8d5ddc8ee6e1

--- main/trait.impl/core/ops/deref/trait.DerefMut.js
+++ main/trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["os",[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"os/sync/up/struct.UPIntrRefMut.html\" title=\"struct os::sync::up::UPIntrRefMut\">UPIntrRefMut</a>&lt;'a, T&gt;",0]]]]);
+    const implementors = Object.fromEntries([["os",[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"os/sync/up/struct.UPIntrRefMut.html\" title=\"struct os::sync::up::UPIntrRefMut\">UPIntrRefMut</a>&lt;'a, T&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
